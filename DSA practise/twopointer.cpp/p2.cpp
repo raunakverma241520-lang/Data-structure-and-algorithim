eb11a9f22e7c27590ad35ef09c32d4feec569ca3
Raunak Verma <raunakverma241520@gmail.com>
2025-12-14 22:45:33 +0530

    Update p2.cpp

--- DSA practise/twopointer.cpp/p2.cpp
+++ DSA practise/twopointer.cpp/p2.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-    vector<int> a = {1,1,2,2,3,3,3,4}; // sample input, modify as needed
+    vector<int> a = {1,1,2,2,3,3,3,4}; 
 
-    int n = a.size();     // size of array
-    int officer = 0;      // slow pointer
-    int cm = 1;           // fast pointer
-    int res = 1;          // result count (unique elements)
+    int n = a.size();    
+    int officer = 0;     
+    int cm = 1;           
+    int res = 1;          
 
     while (cm < n) {
         if (a[cm] == a[cm - 1]) {
@@ -24,3 +24,4 @@ int main() {
     cout << "Unique count = " << res << endl;
     return 0;
 }
+
